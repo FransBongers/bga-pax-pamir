dcfe2f265d52bbbeb6fd09b7c93b5a3502d7cd99
Julien Coignet <breddabasse@hotmail.com>
2022-11-12 14:35:54 +0100

    Typo

--- modules/php/includes/PXPAutoLoader.inc.php
+++ modules/php/includes/PXPAutoLoader.inc.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * CosmosEmpires implementation : © Julien Coignet <breddabasse@hotmail.com>
+ * PaxPamirEditionTwo implementation : © Julien Coignet <breddabasse@hotmail.com>
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
